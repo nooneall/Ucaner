ac31b8c79791bb3cd1760a86ccb91f1913a102ef
Jasonandy <jasonandy@hotmail.com>
2017-08-28 17:17:59 +0800

    1.初始化数据

---
+++ src/cn/ucaner/common/utils/CommonUtil.java
@@ -0,0 +1,35 @@
+/**
+ * <html>
+ * <body>
+ *  <P> Copyright 2014 JasonInternational Ucanx</p>
+ *  <p> All rights reserved.</p>
+ *  <p> Created on 20170828</p>
+ *  <p> Created by Jason</p>
+ *  </body>
+ * </html>
+ */
+package cn.ucaner.common.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+
+import cn.ucaner.framework.config.SystemConfig;
+
+public class CommonUtil {
+	private static String SYSTEM_REQUEST_PATH = "";
+
+	/**
+	 * 获取系统request Scheme
+	 * 
+	 * @return
+	 */
+	public static String getRequestContextPath(HttpServletRequest request) {
+		if (StringUtils.isBlank(SYSTEM_REQUEST_PATH)) {
+			String requestScheme = SystemConfig.getStringValue("system.request.scheme", "https://");
+			SYSTEM_REQUEST_PATH = requestScheme + request.getServerName()
+					+ (80 == request.getServerPort() ? "" : ":" + request.getServerPort());
+		}
+		return SYSTEM_REQUEST_PATH.concat(request.getContextPath());
+	}
+}
