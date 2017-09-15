2dc5fa7182ec11d9c1000496603d4b628b34cb01
Jasonandy <jasonandy@hotmail.com>
2017-09-15 15:29:32 +0800

    1.注解判断有无 2.标志元信息

--- src/cn/ucaner/demo/test/AnnotionOperator.java
+++ src/cn/ucaner/demo/test/AnnotionOperator.java
@@ -13,10 +13,10 @@ public class AnnotionOperator {
         for (Method method : methods) {
             SayHiAnnotation annotationTmp = null;
             if((annotationTmp = method.getAnnotation(SayHiAnnotation.class))!=null){ // 检测是否使用了我们的注解
-            	System.out.println(method.getName());
+            	System.out.println("If:"+method.getName());
                 method.invoke(element,annotationTmp.paramValue()); // 如果使用了我们的注解，我们就把注解里的"paramValue"参数值作为方法参数来调用方法
             }else{
-            	System.out.println(method.getName());
+            	System.out.println("Else:"+method.getName());
                 method.invoke(element, "Rose"); // 如果没有使用我们的注解，我们就需要使用普通的方式来调用方法了
             }
         }
