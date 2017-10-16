b07300290565ae04199d3bd685ef0cd2cacadd63
Jasonandy <jasonandy@hotmail.com>
2017-10-16 11:17:42 +0800

    1.首页跳转实现

--- src/cn/ucaner/base/IndexController.java
+++ src/cn/ucaner/base/IndexController.java
@@ -6,6 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+* @Package：cn.ucaner.base   
+* @ClassName：IndexController   
+* @Description：   <p> index 首页跳转 </p>
+* @Author： - DaoDou 
+* @CreatTime：2017年10月16日 上午11:17:11   
+* @Modify By：   
+* @ModifyTime：  
+* @Modify marker：   
+* @version    V1.0
+ */
 @Controller
 @RequestMapping("/")
 public class IndexController {
