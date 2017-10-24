e038f554a34365eab59a5bbf33a99feafacbdd3a
Jasonandy <jasonandy@hotmail.com>
2017-10-24 11:23:00 +0800

    通用工具类优化

--- src/cn/ucaner/common/utils/CommonUtil.java
+++ src/cn/ucaner/common/utils/CommonUtil.java
@@ -17,33 +17,36 @@ import org.apache.commons.lang3.StringUtils;
 import cn.ucaner.framework.config.SystemConfig;
 
 /**
- * @Package com.sunshine.common.utils
- * @ClassName CommonUtil
- * @Statement
- * 			<p>
- *            </p>
- * @JDK version used: 1.7
- * @Author: 无名子
- * @Create Date: 2016-4-8
- * @modify-Author:
- * @modify-Date:
- * @modify-Why/What:
- * @Version 1.0
+* @Package：cn.ucaner.common.utils   
+* @ClassName：CommonUtil   
+* @Description：   <p> 通用工具类 </p>
+* @Author： - DaoDou 
+* @CreatTime：2017年10月24日 上午11:17:15   
+* @Modify By：   
+* @ModifyTime：  
+* @Modify marker：   
+* @version    V1.0
  */
 public class CommonUtil {
+	
+	/**
+	 * 系统的请求路径 
+	 */
 	private static String SYSTEM_REQUEST_PATH = "";
 
 	/**
-	 * 获取系统request Scheme
-	 * 
+	 * 获取系统request Scheme   by Jason
 	 * @return
 	 */
 	public static String getRequestContextPath(HttpServletRequest request) {
+
 		if (StringUtils.isBlank(SYSTEM_REQUEST_PATH)) {
+			//获取系统的前缀 https  or http 
 			String requestScheme = SystemConfig.getStringValue("system.request.scheme", "https://");
-			SYSTEM_REQUEST_PATH = requestScheme + request.getServerName()
-					+ (80 == request.getServerPort() ? "" : ":" + request.getServerPort());
+			//判断是否为80端口  非80端口 则拼接 https or http + "ServerName:xxx"
+			SYSTEM_REQUEST_PATH = requestScheme + request.getServerName()+ (80 == request.getServerPort() ? "" : ":" + request.getServerPort());
 		}
+		//拼接上下文路径
 		return SYSTEM_REQUEST_PATH.concat(request.getContextPath());
 	}
 }
