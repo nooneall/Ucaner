c4bf213941bc144766eaf7755256c8da2ee8d195
Jason <jason2andy@163.com>
2017-11-19 20:16:35 +0800

    java 8 学习

--- src/cn/ucaner/demo/java801/java8/Employee.java
+++ src/cn/ucaner/demo/java801/java8/Employee.java
@@ -1,3 +1,13 @@
+/**
+ * <html>
+ * <body>
+ *  <P> Copyright 1994 JsonInternational</p>
+ *  <p> All rights reserved.</p>
+ *  <p> Created on 19941115</p>
+ *  <p> Created by Jason</p>
+ *  </body>
+ * </html>
+ */
 package cn.ucaner.demo.java801.java8;
 
 public class Employee {
